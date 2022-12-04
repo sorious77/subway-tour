34079c8a245fc27ba498eec41c478e997866e459
sorious77 <sorious77@naver.com>
2022-12-04 13:12:51 +0900

    404 페이지 구현

---
+++ src/pages/404.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <div className="mb-4 text-2xl">요청하신 페이지를 찾을 수 없습니다.</div>
+      <button
+        className="px-4 py-2 text-white bg-black rounded dark:text-black dark:bg-white"
+        onClick={() => router.push("/")}
+      >
+        홈으로
+      </button>
+    </div>
+  );
+};
+
+export default NotFound;
