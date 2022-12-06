7a9947ca4a89910b4eb85af23fa69ae4fbf3366b
sorious77 <sorious77@naver.com>
2022-12-06 15:09:54 +0900

    dotenv 설치

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const Dotenv = require("dotenv-webpack");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(new Dotenv({ silent: true }));
+
+    return config;
+  },
+};
