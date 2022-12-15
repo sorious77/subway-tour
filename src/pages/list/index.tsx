0ac42c30d87f9fb486b5d073fe67712dfd79ecc6
sorious77 <sorious77@naver.com>
2022-12-15 22:43:13 +0900

    포스트 목록 API 적용

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,5 +1,4 @@
 import Post from "components/Post";
-import loadConfig from "next/dist/server/config";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -10,45 +9,44 @@ interface Post {
   thumbnail?: string;
 }
 
-interface PostInfo {
-  posts: Post[];
-  pages: number;
-}
-
 const List = () => {
   const router = useRouter();
-  const [postInfo, setPostInfo] = useState<PostInfo>();
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     (async () => {
-      if (router.isReady) {
-        const page = router.query.page;
+      const result = await (await fetch(`/api/list/${currentPage}`)).json();
 
-        const result = await (await fetch(`/api/list/${page}`)).json();
-
-        setPostInfo(result);
-      }
+      setPosts([...posts, ...result]);
     })();
-  }, [router.isReady]);
+  }, [currentPage]);
 
   return (
     <div className="flex flex-col items-center justify-center px-4">
       <button
         onClick={() => {
-          router.push("/write");
+          //router.push("/write");
+          setCurrentPage((prev) => prev + 1);
         }}
         className="self-end px-4 my-4 mr-2 text-white bg-black rounded-2xl dark:bg-white dark:text-black"
       >
         글쓰기
       </button>
-      {postInfo ? (
-        <>
-          {postInfo.posts.map((post) => (
-            <Post key={post.id} {...post} />
+      {posts?.length > 0 ? (
+        <div className="w-full">
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              title={post.title}
+              id={post.id}
+              content={post.content}
+            />
           ))}
-        </>
+        </div>
       ) : (
-        "Loading..."
+        <span>Loading...</span>
       )}
     </div>
   );
