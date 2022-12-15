560d0286d93b81e69bce9fe20b73f4c68496d7bb
sorious77 <sorious77@naver.com>
2022-12-15 23:16:08 +0900

    글쓰기 버튼 수정

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -30,9 +30,22 @@ const List = () => {
           //router.push("/write");
           setCurrentPage((prev) => prev + 1);
         }}
-        className="self-end px-4 my-4 mr-2 text-white bg-black rounded-2xl dark:bg-white dark:text-black"
+        className="fixed flex items-center self-end p-4 text-white bg-black rounded-full right-10 bottom-10 dark:bg-white dark:text-black"
       >
-        글쓰기
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
       </button>
       {posts?.length > 0 ? (
         <div className="w-full">
