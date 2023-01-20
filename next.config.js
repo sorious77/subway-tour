b46d636ee291acbfbbe57fb67a565bf583de4d0e
sorious77 <sorious77@naver.com>
2023-01-20 21:32:16 +0900

    config 수정

--- next.config.js
+++ next.config.js
@@ -4,12 +4,11 @@ const Dotenv = require("dotenv-webpack");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = {
   webpack: (config) => {
     config.plugins.push(new Dotenv({ silent: true }));
 
     return config;
   },
 };
+
+module.exports = nextConfig;
