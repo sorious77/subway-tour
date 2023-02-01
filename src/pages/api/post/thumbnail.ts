fc0da213bc8b4f974f4ee03915eef6670bf3e4a1
sorious77 <sorious77@naver.com>
2023-02-01 23:46:00 +0900

    비동기 코드 수정

--- src/pages/api/post/thumbnail.ts
+++ src/pages/api/post/thumbnail.ts
@@ -3,34 +3,28 @@ import Handler from "../../../libs/Handler";
 import axios from "axios";
 import formidable from "formidable";
 
-const ThumbnailHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const filePromise = new Promise<File>((resolve, reject) => {
-      const form = new formidable.IncomingForm({
-        // maxFileSize: 5 * 1024 * 1024,
-        // keepExtensions: true
-      });
-
-      form.parse(req, (err, _, files) => {
-        const file: File = Array.isArray(files) ? files[0] : files;
-
-        if (err) return reject(err);
-        return resolve(file);
-      });
+const receiveFile = (req: NextApiRequest) =>
+  new Promise<File>((resolve, reject) => {
+    const form = new formidable.IncomingForm({
+      // maxFileSize: 5 * 1024 * 1024,
+      // keepExtensions: true
     });
 
-    let fileData: File;
-    filePromise
-      .then((file) => {
-        fileData = file;
-      })
-      .catch((error) => {
-        throw new Error("");
-      });
+    form.parse(req, (err, _, files) => {
+      const file: File = Array.isArray(files) ? files[0] : files;
+
+      if (err) return reject(err);
+      return resolve(file);
+    });
+  });
+
+const ThumbnailHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const file = await receiveFile(req);
 
     const formData = new FormData();
 
-    formData.append("image", fileData!);
+    formData.append("image", file);
 
     const result = await axios.post(
       `${process.env.BASE_URL}/thumbnail`,
@@ -42,48 +36,11 @@ const ThumbnailHandler = async (req: NextApiRequest, res: NextApiResponse) => {
       }
     );
 
-    // const result = await axios({
-    //   url: `${process.env.BASE_URL}/thumbnail`,
-    //   data: formData,
-    //   method: "post",
-    // });
-
     console.log("result", result);
-
     res.send("success");
-
-    // const fileData: formidable.Files = await new Promise((resolve, reject) => {
-    //   const form = new formidable.IncomingForm({
-    //     maxFileSize: 5 * 1024 * 1024,
-    //     keepExtensions: true,
-    //     maxFiles: 1,
-    //   });
-
-    //   form.parse(req, (err, fields, files) => {
-    //     if (err) return reject(err);
-
-    //     return resolve(files);
-    //   });
-    // });
-
-    // const formData = new FormData();
-    // const file = Array.isArray(fileData.file)
-    //   ? fileData.file[0]
-    //   : fileData.file;
-
-    // // formData.append("thumbnail", file);
-
-    // const url =
-    //   "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg";
-
-    // return res.status(200).json({ url });
-
-    // const result = await axios({
-    //     url:`${process.env.BASE_URL}/posts/thumbnail`,
-    //     data: req.body,
-    //     method: "post"
-    // })
-  } catch (e) {}
+  } catch (e) {
+    res.json({ e });
+  }
 };
 
 export const config = {
