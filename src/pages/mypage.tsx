ab2d80b61dd2293f8aa5a97212e7aec0849db618
sorious77 <sorious77@naver.com>
2023-03-19 16:32:39 +0900

    mypage 수정

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,54 +1,69 @@
-import { useForm } from "react-hook-form";
-
-type InputValue = {
-  thumbnail: FileList;
-};
+import { useRecoilValue } from "recoil";
+import { userState } from "components/states";
+import { useRouter } from "next/router";
 
 const MyPage = () => {
-  const { register, handleSubmit, watch } = useForm<InputValue>();
-
-  const watchThumbnail = watch("thumbnail");
-
-  const uploadThumbnail = async (data: InputValue) => {
-    if (watchThumbnail && watchThumbnail.length > 0) {
-      console.log("thumbnail exists");
-    } else {
-      return;
-    }
-
-    const thumbnail = data?.thumbnail[0];
-
-    const formData = new FormData();
-    formData.append("image", thumbnail);
-
-    try {
-      const result = await fetch("/api/post/thumbnail", {
-        method: "POST",
-        body: formData,
-      });
-
-      console.log(result);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  const user = useRecoilValue(userState);
+  const router = useRouter();
 
   return (
-    <div>
-      <h1>MyPage</h1>
-      <form
-        onSubmit={handleSubmit(async (data) => {
-          await uploadThumbnail(data);
-        })}
-      >
-        <input
-          type="file"
-          className="h-16 mt-2 text-center"
-          {...register("thumbnail")}
-          accept="image/*"
-        />
-        <input type="submit" value="Submit" />
-      </form>
+    <div className="flex flex-col items-center h-full">
+      <div className="flex flex-col items-center">
+        <div className="text-2xl flex items-center mb-10">
+          <div className="mr-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-10 h-10 text-rose-400"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          {user?.nickname}
+        </div>
+        <div className="flex flex-col">
+          <div
+            className="flex items-center mb-4 cursor-pointer"
+            onClick={() => router.push("/user/update")}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 mr-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+            정보 수정
+          </div>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 mr-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+              />
+            </svg>
+            내가 쓴 글
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
